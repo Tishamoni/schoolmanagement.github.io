ada64dce128097ff367ebc78a4512e4a8c261eef
Tishamoni <86648679+Tishamoni@users.noreply.github.com>
2022-01-12 23:45:47 +0600

    my school website

---
+++ School-Management-System-master/School-Management-System-master/README.md
@@ -0,0 +1,70 @@
+# School Management System
+
+This is a MERN stack website which can be used for managing the school. It has almost all features and few are in development phase.
+
+`Please give this repo a star if this project helped you in any way. This will mean a lot to me and motivate me more to bring this type of project.`
+
+### `LIVE SITE`
+
+```bash
+https://manageschool1.herokuapp.com/
+
+``` 
+
+### `LOGIN AS ADMIN AND SEE EVERYTHING`
+
+```bash
+Login email="admin1@example.com"
+Login password="123456"
+```
+
+## Usage
+
+Clone the repo
+
+```bash
+git clone https://github.com/upendradhamala/School-Management-System.git
+```
+
+### Getting Started
+
+Go to the root folder and install the backend dependencies by using the command-
+
+```bash
+npm install
+```
+
+Go to the frontend folder by command cd frontend and then install frontend dependecies using the command-
+
+```bash
+npm install
+```
+
+### `ENV Variables`
+
+Create a .env file in the root and add the following
+
+```bash
+NODE_ENV=development
+PORT=5000
+MONGO_URI="Your mongo uri"
+JWT_SECRET="Anything you like"
+CLOUDINARY_URL ="your cloudinary url"
+CLOUDINARY_UPLOAD_PRESET = "your cloudinary preset "
+```
+
+### `Running`
+
+```bash
+Go to the root folder
+npm run dev
+
+```
+
+### `Homepage`
+
+[![Screenshot-from-2021-04-30-16-59-15.png](https://i.postimg.cc/jjHs8psH/Screenshot-from-2021-04-30-16-59-15.png)](https://postimg.cc/67QJq14q)
+
+
+
+
---
+++ School-Management-System-master/School-Management-System-master/frontend/src/App.js
@@ -0,0 +1,119 @@
+import React, { useState } from 'react'
+import Landing from './screens/Landing'
+// import
+// import { Button } from 'react-bootstrap'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './screens/Login'
+import StudentRegister from './screens/StudentRegister'
+import StudentFees from './screens/StudentFees'
+import StudentDetails from './screens/StudentDetails'
+import StudentDeepDetails from './screens/StudentDeepDetails'
+import StudentAttendance from './screens/StudentAttendance'
+import AllStudents from './screens/AllStudents'
+import StudentDeepAttendance from './screens/StudentDeepAttendance'
+import StudentAdmitCard from './screens/StudentAdmitCard'
+import AllStudentsAdmitCard from './screens/AllStudentsAdmitCard'
+import StudentClassAdmitCard from './screens/StudentClassAdmitCard'
+import StudentClassAdmitCardDeep from './screens/StudentClassAdmitCardDeep'
+import ParticularStudentAdmitCard from './screens/ParticularStudentAdmitCard'
+import TeacherSalary from './screens/TeacherSalary'
+import StaffSalary from './screens/StaffSalary'
+import TeacherRegister from './screens/TeacherRegister'
+import AllTeachers from './screens/AllTeachers'
+import AllStaffs from './screens/AllStaffs'
+import StaffRegister from './screens/StaffRegister'
+import IncomeScreen from './screens/IncomeScreen'
+import ExpenseScreen from './screens/ExpenseScreen'
+import underConstruction from './components/underConstruction'
+import { studentAttendances } from './actions/studentActions'
+// import ExpenseScreen from './screens/ExpenseScreen'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route path='/' component={Landing} exact />
+          <Route path='/login' component={Login} exact />
+          <Route path='/student-register' component={StudentRegister} />
+          <Route path='/student-fee' component={StudentFees} />
+          <Route path='/student_details' component={StudentDetails} exact />
+          <Route
+            path='/student_details/details/:id'
+            component={StudentDeepDetails}
+            exact
+          />
+          {/* <Route
+            path='/student-attendance'
+            component={underConstruction}
+            exact
+          /> */}
+          <Route
+            path='/teacher_attendance'
+            component={underConstruction}
+            exact
+          />
+          <Route
+            path='/non-teaching_staff_attendance'
+            component={underConstruction}
+            exact
+          />
+          <Route
+            path='/student-attendance/:class'
+            component={StudentDeepAttendance}
+            exact
+          />
+          <Route path='/students' component={AllStudents} exact />
+          <Route path='/admit_card' component={StudentAdmitCard} exact />
+          <Route
+            path='/admit_card/allstudents'
+            component={AllStudentsAdmitCard}
+            exact
+          />
+          <Route
+            path='/admit_card/classes'
+            component={StudentClassAdmitCard}
+            exact
+          />
+          <Route
+            path='/admit_card/classes/:id'
+            component={StudentClassAdmitCardDeep}
+            exact
+          />
+          <Route
+            path='/admit_card/student'
+            component={ParticularStudentAdmitCard}
+            exact
+          />
+          <Route path='/teacher_salary' component={TeacherSalary} exact />
+          <Route path='/teacher_register' component={TeacherRegister} exact />
+          <Route path='/teacher_details' component={AllTeachers} exact />
+          <Route
+            path='/non-teaching_staff_details'
+            component={AllStaffs}
+            exact
+          />
+          <Route
+            path='/non-teaching_staff_register'
+            component={StaffRegister}
+            exact
+          />
+          <Route
+            path='/non-teaching_staff_salary'
+            component={StaffSalary}
+            exact
+          />
+          <Route path='/income' component={IncomeScreen} exact />
+          <Route path='/salary' component={ExpenseScreen} exact />
+          <Route
+            path='/student-attendance'
+            component={StudentAttendance}
+            exact
+          />
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App
